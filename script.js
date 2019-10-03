360eb8ec0cb100616c6781ca12fd504a9ba104d5
Simeon Wordlaw <simeon.wordlaw@gmail.com>
2019-10-02 22:12:05 -0500

    Create script.js
    email validation

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+function validate() {
+    var email = document.getElementById('email').value;
+    alert('Email entered is ' + email);
+    if (!validateEmail(email)) {
+      alert('Invalid Email Entered');
+    }
+    return true;
+}
